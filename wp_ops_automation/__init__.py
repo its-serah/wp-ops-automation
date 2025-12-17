193a52fe82082e3bd5dbfacac0b23847ab0a1d25
Serah <serah.elrashidi@gmail.com>
2025-12-17 12:58:11 +0200

    chore: remove legacy WordPress package

--- wp_ops_automation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core package for the wp-ops-automation toolkit.
-
-This is currently just boilerplate and a place to hang
-shared types, utilities, and configuration.
-"""
-
--- wp_ops_automation/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Task modules for reusable WordPress automation flows.
-
-Add new modules here as your automation surface grows.
-"""
-
